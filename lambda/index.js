21698071c34d6a75ad99eaf4317db0233d358584
Ronny <rangel.ronny8@gmail.com>
2019-10-23 23:53:34 -0500

    add values in the recordRide funtions

--- lambda/index.js
+++ lambda/index.js
@@ -26,7 +26,27 @@ function recordRide(survey) {
         Item: {
             email: survey.email,
             feeling: survey.feeling,
-            pensiones: survey.pensiones,
+            pensiones: survey.according-degree-satisfaction-afp-pensions,
+            impuestos: survey.according-degree-satisfaction-income-tax,
+            sueldos: survey.according-degree-satisfaction-salaries,
+            servicios: survey.according-degree-satisfaction-basic-services,
+            educacion: survey.according-degree-satisfaction-education,
+            transporte: survey.according-degree-satisfaction-transport,
+            salud: survey.according-degree-satisfaction-health,
+            seguridad_delicuencia: survey.according-degree-satisfaction-security-crime,
+            otros: survey.according-degree-satisfaction-others,
+            tema_relevante: survey.relevant-topic,
+            responsable_situacion_actual: survey.responsible-current-situation,
+            manifestaciones: survey.social-manifestation,
+            regiones: survey.regiones,
+            comunas: survey.comunas,
+            genero: survey.gender,
+            edad: survey.age,
+            evaluacion_poblacion_civil: survey.civil-population-assessment,
+            evaluacion_carabineros: survey.carabineros-evaluation,
+            evaluacion_ejercito: survey.army-evaluation,
+            evaluacion_medios: survey.media-evaluation,
+            iniciativa: survey.project-or-initiative
         },
     }).promise();
 }
