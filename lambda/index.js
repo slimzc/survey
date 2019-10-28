a156c88747b93847309933cfd71526fa86f8c503
Ronny <rangel.ronny8@gmail.com>
2019-10-28 17:36:07 -0500

    transform body survey to JSON.stringfy

--- lambda/index.js
+++ lambda/index.js
@@ -8,25 +8,26 @@ exports.handler = (event, context, callback) => {
 
     recordRide(survey).then(() => {
         callback(null, {
-            statusCode: 201,
-            body: survey,
+        statusCode: 201,
+            body: JSON.stringify(survey),
             headers: {
-                'Access-Control-Allow-Origin': '*',
-                "Access-Control-Allow-Credentials" : true
+            'Access-Control-Allow-Origin': '*',
+            "Access-Control-Allow-Credentials" : true 
             },
-        });
-    }).catch((err) => {
-        console.error(err);
-        errorResponse(err.message, context.awsRequestId, callback)
     });
+}).catch((err) => {
+        console.error(err);
+    errorResponse(err.message, context.awsRequestId, callback)
+});
 };
 //TODO: Complete thus item with the survey values :)
 function recordRide(survey) {
+    console.log(survey);
     return ddb.put({
         TableName: 'survey',
         Item: {
             email: survey.email,
-            feeling: survey.feeling,
+            feeling: (survey.feeling != '' || survey.feeling == undefined) ? survey.feeling:'N/A',
             degreeSatisfactionAfpPensions: survey.according_degree_satisfaction_afp_pensions,
             degreeSatisfactionIncomeTax: survey.according_degree_satisfaction_income_tax,
             degreeSatisfactionSalaries: survey.according_degree_satisfaction_salaries,
@@ -35,19 +36,21 @@ function recordRide(survey) {
             degreeSatisfactionTransport: survey.according_degree_satisfaction_transport,
             degreeSatisfactionHealth: survey.according_degree_satisfaction_health,
             degreeSatisfactionSecurityCrime: survey.according_degree_satisfaction_security_crime,
-            degreeSatisfactionOthers: survey.according_degree_satisfaction_others,
-            relevantTopic: survey.relevant_topic,
-            responsableSituacionActual: survey.responsible_current_situation,
-            manifestaciones: survey.social_manifestation,
+            degreeSatisfactionOthers: (survey.according_degree_satisfaction_others != '' || survey.according_degree_satisfaction_others == undefined) ? survey.according_degree_satisfaction_others:'N/A',
+            relevantTopic: (survey.relevant_topic != '' || survey.relevant_topic == undefined) ? survey.relevant_topic:'N/A',
+            responsableSituacionActual: (survey.responsible_current_situation != '' || survey.responsible_current_situation == undefined) ? survey.responsible_current_situation:'N/A',
+            manifestaciones: (survey.social_manifestation != '' || survey.social_manifestation == undefined) ? survey.social_manifestation:'N/A',
+            evasionLegitimateMethod: (survey.evasion_legitimate_method != '' || survey.evasion_legitimate_method == undefined) ? survey.evasion_legitimate_method:'N/A',
+            violenceLegitimateMethod: (survey.violence_legitimate_method != '' || survey.violence_legitimate_method == undefined) ? survey.violence_legitimate_method:'N/A',
             regiones: survey.regiones,
             comunas: survey.comunas,
-            genero: survey.gender,
-            edad: survey.age,
+            genero: (survey.gender != '' || survey.gender == undefined) ? survey.gender:'N/A',
+            edad: (survey.age != '' || survey.age == undefined) ? survey.age:'N/A',
             evaluacionPoblacionCivil: survey.civil_population_assessment,
             evaluacionCarabineros: survey.carabineros_evaluation,
             evaluacionEjercito: survey.army_evaluation,
             evaluacionMedios: survey.media_evaluation,
-            iniciativa: survey.project_or_initiative
+            iniciativa: (survey.project_or_initiative != '' || survey.project_or_initiative == undefined) ? survey.project_or_initiative:'N/A',
         },
     }).promise();
 }
@@ -61,7 +64,7 @@ function errorResponse(errorMessage, awsRequestId, callback) {
         }),
         headers: {
             'Access-Control-Allow-Origin': '*',
-            "Access-Control-Allow-Credentials" : true
+            "Access-Control-Allow-Credentials" : true 
 
         },
     });
